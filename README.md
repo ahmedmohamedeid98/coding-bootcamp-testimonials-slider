3b98732b64a6f8f05ac41cd477f327c5549b9b50
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2022-01-11 03:51:59 +0200

    complete the challenge

---
+++ README.md
@@ -0,0 +1,86 @@
+# Frontend Mentor - Coding bootcamp testimonials slider solution
+
+## Welcome! 👋
+
+Thanks for checking out this 
+
+### Screenshot
+
+![](./design/desktop-design-slide-1.jpg)
+
+
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Your challenge is to build out this testimonial slider and get it looking as close to the design as possible.
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- bootstrap 5
+- javascript
+- Mobile-first workflow
+
+### What I learned
+
+- Positioning
+- how to order elements on the row
+
+```css
+.elementOne {
+  order: 1;
+}
+
+.elementTwo {
+  order: 0;
+}
+
+```
+- how to overlabe one column two another one.
+
+```css
+/*ex: overlap element two on element one*/
+.elementTwo {
+  margin-right: -100px;
+}
+
+```
+
+### Useful resources
+
+- [W3school](https://www.w3schools.com/jsref/prop_img_src.asp)
+- [getcssscan](https://getcssscan.com/css-box-shadow-examples) - gives a lot of box-shadow examples.
+
+
+## Author
+
+- Frontend Mentor - [@ahmed eid](https://www.frontendmentor.io/profile/ahmedmohamedeid98)
+- Linkedin - [@ahmed eid](https://www.linkedin.com/in/ahmedmohamedeid/)
---
+++ js/index.js
@@ -0,0 +1,53 @@
+class Student {
+    constructor(id, imageUrl, quote, name, job) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.quote = quote;
+        this.name = name;
+        this.job = job;
+    }
+};
+
+const students = [
+    new Student(
+        1, '../images/image-john.jpg',
+          "“If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.”",
+          "John Tarkpor", "Junior Front-end Developer"),
+    new Student(
+        1, '../images/image-tanya.jpg',
+        "I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.",
+        "Tanya Sinclair", "UX Engineer")
+];
+
+const prev      = document.getElementById("prev");
+const next      = document.getElementById("next");
+const quote     = document.getElementById("quote-txt");
+const _name     = document.getElementById("name");
+const _job      = document.getElementById("job");
+const img       = document.getElementById("std-img");
+
+// init ui with first student data
+publishData(0); 
+
+var currentIndex = 0;
+const len = students.length;
+
+// listeners
+prev.addEventListener("click", (_) => {
+    currentIndex = (currentIndex + len - 1) % len;
+    publishData(currentIndex);
+});
+
+next.addEventListener("click", (_) => {
+    currentIndex = (currentIndex + 1) % len;
+    publishData(currentIndex);
+});
+
+
+// publish data to ui
+function publishData(index) {
+    quote.textContent = students[index].quote;
+    _name.textContent = students[index].name;
+    _job.textContent  = students[index].job;
+    img.src = students[index].imageUrl;
+}
