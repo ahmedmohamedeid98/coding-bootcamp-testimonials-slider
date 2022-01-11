461218d9640964b45e81732636578b733ca0b455
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2022-01-11 04:22:16 +0200

    fix image path bug

--- js/index.js
+++ js/index.js
@@ -10,11 +10,11 @@ class Student {
 
 const students = [
     new Student(
-        1, '../images/image-john.jpg',
+        1, './images/image-john.jpg',
           "“If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.”",
           "John Tarkpor", "Junior Front-end Developer"),
     new Student(
-        1, '../images/image-tanya.jpg',
+        1, './images/image-tanya.jpg',
         "I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.",
         "Tanya Sinclair", "UX Engineer")
 ];
